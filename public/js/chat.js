a641a0496fbb6c0bf39ccb6c6f7a4799212fd5ed
David Phillips <david.phillips@visionistinc.com>
2015-07-29 12:57:45 -0400

    Usernames now displayed in chat #47
    The chat pane now displays the usernames of other people in the chat below their message. This does not display the username of mine by design in order to prevent clutter.

--- public/js/chat.js
+++ public/js/chat.js
@@ -68,16 +68,21 @@ function Chat (data) {
 
     $("#typing-" + message.authorId).remove ();
     var classes = "chat-message";
+    var userDiv;
 
     if (message.authorId == instance.client.id) {
       classes += " bubble-mine animated bounceIn";
+      // userClasses = "chat-user bubble-mine animated bounceIn"
       chatdiv  = $('<div>').addClass (classes).text (message.message).css ('background-color', instance.client.color);
+      // userDiv = $('<div>').addClass(userClasses).text(message.authorId);
     } else {
       if (!prepend) {
         instance.sendNotification (message);
       }
       classes += " bubble-other animated bounceIn";
+      userClasses = "chat-user bubble-other animated bounceIn"
       chatdiv  = $('<div>').addClass (classes).text (message.message).css ('background-color', message.color);
+      userDiv = $('<div>').addClass(userClasses).text(message.authorId);
   	}
 
     /*
@@ -91,9 +96,18 @@ function Chat (data) {
     var shouldScroll = Math.abs (chatpane.scrollHeight - ($(chatpane).scrollTop () + $(chatpane).height ()));
 
     if (prepend) {
+      if(typeof userDiv !== "undefined"){
+         $('#chat-pane').prepend(userDiv);
+       }
       $('#chat-pane').prepend (chatdiv);
+
     } else {
+
       $('#chat-pane').append (chatdiv);
+
+      if(typeof userDiv !== "undefined"){
+        $('#chat-pane').append(userDiv);
+       }
     }
 
     /*
